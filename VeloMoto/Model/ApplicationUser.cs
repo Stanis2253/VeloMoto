08b0dc4c40415dd8e17d4191d9713d64efc67bc3
stanis2253 <stanis2253@gmail.com>
2023-03-13 13:34:10 +0300

    AddModelAndDbContext

---
+++ VeloMoto/Model/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Patronymic { get; set; }
+
+        [NotMapped]
+        private string fullName;
+        [NotMapped]
+        public string FullName
+        {
+            get { return fullName = (FirstName +" " + LastName + " " + Patronymic); }
+            set { fullName = value; }
+        }
+
+
+    }
+}
---
+++ VeloMoto/Model/Price.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Price
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        public decimal Value { get; set; }
+
+        private DateTime dateTime;
+
+        public DateTime DateTime
+        {
+            get { return dateTime = DateTime.Now; }
+
+            set { dateTime = value; }
+        }
+    }
+}
